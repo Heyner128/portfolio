f89549a998db45f290f5842d04251b2235fd1089
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2022-06-18 00:09:34 +0200

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("views"));
 
 app.get('/', function (req, res) {
   try {
