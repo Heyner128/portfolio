ccf543f1da546669af3b16da3d06342ab44b5cea
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2022-07-06 23:04:00 +0200

    updates

--- views/main.js
+++ views/main.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
-    document.querySelector("nav ul").classList.toggle("scale-x-0");
-    document.querySelector("nav ul").classList.toggle("w-0");
+    document.querySelector("#sidebar-content").classList.toggle("scale-x-0");
+    document.querySelector("#sidebar-content").classList.toggle("w-0");
+
+    document.querySelector("#sidebar").classList.toggle("w-full");
+    document.querySelector("#sidebar").style.backgroundColor = "RGBA(0,0,0,0.4)";
+    document.querySelector("#sidebar div").classList.toggle("shadow-md")
 }
